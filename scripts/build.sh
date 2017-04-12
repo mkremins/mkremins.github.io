7d1ae98448e640fc69aea7714fcf1d8244f9a9da
Max Kreminski <maxkreminski@gmail.com>
2017-04-12 10:35:26 -0700

    Add projects page

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 # compile CSS using PostCSS
+
 postcss \
   --use autoprefixer \
   --use cssnano \
   --output ./css/styles.min.css \
   ./css/styles.css
+
+postcss \
+  --use autoprefixer \
+  --use cssnano \
+  --output ./css/projects.min.css \
+  ./css/projects.css
