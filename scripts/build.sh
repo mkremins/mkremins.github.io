023e126e7986953eb43c75a553761f0d08a77a06
Max Kreminski <maxkreminski@gmail.com>
2021-03-20 14:57:53 -0700

    Cleanup: remove projects page CSS from CSS compile script
    Probably going to remove this whole script soon.

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,9 +7,3 @@ postcss \
   --use cssnano \
   --output ./css/styles.min.css \
   ./css/styles.css
-
-postcss \
-  --use autoprefixer \
-  --use cssnano \
-  --output ./css/projects.min.css \
-  ./css/projects.css
